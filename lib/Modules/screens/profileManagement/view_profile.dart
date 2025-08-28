e233fc1251ff8c405198c7a6a7a6195bce01b211
Aashish Chauhan <aashishrajput782@gmail.com>
2025-08-28 17:20:51 +0530

    get address API Implemented

--- lib/Modules/screens/profileManagement/view_profile.dart
+++ lib/Modules/screens/profileManagement/view_profile.dart
@@ -35,7 +35,7 @@ class ViewProfile extends ParentWidget {
               ),
               16.kH,
               commonCards("First Name", controller.profileData.value.data?.firstName ?? "", Icons.person_outline),
-              commonCards("First Name", controller.profileData.value.data?.lastName ?? "", Icons.person_outline),
+              commonCards("Last Name", controller.profileData.value.data?.lastName ?? "", Icons.person_outline),
               commonCards("Phone Number", "${controller.profileData.value.data?.countryCode ?? ""} ${controller.profileData.value.data?.phoneNo ?? ""}", Icons.phone_outlined),
               if (controller.profileData.value.data?.email != null) commonCards("Email", controller.profileData.value.data?.email ?? "", Icons.email_outlined),
               commonCards("Caste Category", "${parseUserCasteCategory(controller.profileData.value.data?.userCasteCategory)?.displayName}  ( ${controller.profileData.value.data?.subCaste} )", Icons.people_outline),
