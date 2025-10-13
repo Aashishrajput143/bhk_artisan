a41d8267775a5263bf805b40f20faf8d1f45761b
Aashish Chauhan <aashishrajput782@gmail.com>
2025-10-13 17:50:47 +0530

    view profile expertise chips implement

--- lib/Modules/screens/profileManagement/view_profile.dart
+++ lib/Modules/screens/profileManagement/view_profile.dart
@@ -57,11 +57,11 @@ class ViewProfile extends ParentWidget {
               Row(
                 children: [
                   Expanded(flex: 1, child: commonCards(appStrings.category, "${parseUserCasteCategory(controller.profileData.value.data?.userCasteCategory)?.displayName}", Icons.groups_outlined)),
-                  Expanded(flex: 1, child: commonCards(appStrings.caste, controller.profileData.value.data?.subCaste??"", Icons.people_outline)),
+                  Expanded(flex: 1, child: commonCards(appStrings.caste, controller.profileData.value.data?.subCaste ?? "", Icons.people_outline)),
                 ],
               ),
               if (controller.profileData.value.data?.gstNumber != null) commonCards(appStrings.gstNumber, controller.profileData.value.data?.gstNumber ?? "", Icons.business),
-              commonCards(appStrings.expertise, controller.profileData.value.data?.expertizeField ?? appStrings.pleaseSetExpertise, Icons.work_outline),
+              commonChipsCards(appStrings.expertise,controller.profileData.value.data?.expertizeField?.split(',') ?? [], Icons.work_outline),
             ],
           ),
         ),
@@ -70,18 +70,38 @@ class ViewProfile extends ParentWidget {
   }
 
   String formatAadhaarNumber(String aadhaar) {
-      if (aadhaar.isEmpty) return "";
-      aadhaar = aadhaar.replaceAll(RegExp(r'\s+'), ''); // remove spaces if any
+    if (aadhaar.isEmpty) return "";
+    aadhaar = aadhaar.replaceAll(RegExp(r'\s+'), ''); // remove spaces if any
 
-      String formatted = '';
-      for (int i = 0; i < aadhaar.length; i++) {
-        formatted += aadhaar[i];
-        if ((i + 1) % 4 == 0 && i + 1 != aadhaar.length) {
-          formatted += ' ';
-        }
+    String formatted = '';
+    for (int i = 0; i < aadhaar.length; i++) {
+      formatted += aadhaar[i];
+      if ((i + 1) % 4 == 0 && i + 1 != aadhaar.length) {
+        formatted += ' ';
       }
-      return formatted;
     }
+    return formatted;
+  }
+
+  Widget commonChipsCards(String title, List expertiseList, IconData icon) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 6),
+      child: ListTile(
+        leading: Icon(icon, color: Colors.black),
+        title: Text(title, style: TextStyle(color: Colors.grey)),
+        subtitle: Wrap(
+          spacing: 6.0,
+          children: expertiseList.map((e) {
+            return Chip(
+              label: Text(e.trim(), style: const TextStyle(fontSize: 14, color: Colors.white)),
+              backgroundColor: appColors.brownbuttonBg,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
 
   Widget commonCards(String title, String subtitle, IconData icon) {
     return Padding(
