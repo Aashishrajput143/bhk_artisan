de9d99436db5f738bafae01d4141d17a4ca777e4
Aashish Chauhan <aashishrajput782@gmail.com>
2025-10-31 11:43:28 +0530

    logistics tracking completed

--- lib/Modules/screens/support_screen.dart
+++ lib/Modules/screens/support_screen.dart
@@ -42,7 +42,7 @@ class SupportScreen extends ParentWidget {
                       controller.messageController.value,
                       controller.messageFocusNode.value,
                       w,
-                      maxLines:h<700?16: controller.messageError.value?.isNotEmpty??false?19: 20,
+                      maxLines:h<700?12: controller.messageError.value?.isNotEmpty??false?19:h>900?20: 16,
                       minLines: 12,
                       error: controller.messageError,
                       onChange: (value) {
